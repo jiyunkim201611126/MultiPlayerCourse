54b18fab78377ad9899cb9b8378b4eba6902993f
jiyunkim201611126 <solt0228@gmail.com>
2025-02-01 02:07:52 +0900

    섹션 4-78 완료
    Projectile 발사 구현
    Projectile HitEvent 구현
    Projectile 파티클 구현
    점프 시 총구가 너무 왼쪽을 바라보던 현상 수정
    다음에 다시 읽기 쉽도록 각종 주석 멘트

--- Blaster/Source/Blaster/Weapon/ProjectileWeapon.cpp
+++ Blaster/Source/Blaster/Weapon/ProjectileWeapon.cpp
@@ -1,2 +1,40 @@
 #include "ProjectileWeapon.h"
 
+#include "Engine/SkeletalMeshSocket.h"
+#include "Projectile.h"
+
+void AProjectileWeapon::Fire(const FVector& HitTarget)
+{
+	// 격발 애니메이션 재생
+	Super::Fire(HitTarget);
+
+	APawn* InstigatorPawn = Cast<APawn>(GetOwner());
+
+	// 총구쪽 Socket
+	const USkeletalMeshSocket* MuzzleFlashSocket = GetWeaponMesh()->GetSocketByName(FName("MuzzleFlash"));
+	if (MuzzleFlashSocket)
+	{
+		// LineTrace 시작 Transform(== 총구쪽 Socket의 Transform)
+		FTransform SocketTransform = MuzzleFlashSocket->GetSocketTransform(GetWeaponMesh());
+		// 총구에서 HitTarget으로의 방향 계산
+		FVector ToTarget = HitTarget - SocketTransform.GetLocation();
+		FRotator TargetRotation = ToTarget.Rotation();
+		if (ProjectileClass && InstigatorPawn)
+		{
+			// ProjectileClass를 기반으로 한 액터 스폰
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.Owner = GetOwner();
+			SpawnParams.Instigator = InstigatorPawn;
+			UWorld* World = GetWorld();
+			if (World)
+			{
+				World->SpawnActor<AProjectile>(
+					ProjectileClass,
+					SocketTransform.GetLocation(),
+					TargetRotation,
+					SpawnParams
+				);
+			}
+		}
+	}
+}
