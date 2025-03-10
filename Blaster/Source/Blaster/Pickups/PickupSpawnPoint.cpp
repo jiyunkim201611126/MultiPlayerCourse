367848e395378a80f4ed66a1c814ba0d39244fbf
jiyunkim201611126 <103517141+jiyunkim201611126@users.noreply.github.com>
2025-03-11 00:52:16 +0900

    section 11-169 in progress
    Spawn pickup

---
+++ Blaster/Source/Blaster/Pickups/PickupSpawnPoint.cpp
@@ -0,0 +1,52 @@
+﻿#include "PickupSpawnPoint.h"
+
+#include "Pickup.h"
+
+APickupSpawnPoint::APickupSpawnPoint()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+}
+
+void APickupSpawnPoint::BeginPlay()
+{
+	Super::BeginPlay();
+	StartSpawnPickupTimer((AActor*)nullptr);
+}
+
+void APickupSpawnPoint::StartSpawnPickupTimer(AActor* DestroyedActor)
+{
+	GetWorldTimerManager().SetTimer(
+		SpawnPickupTimer,
+		this,
+		&ThisClass::SpawnPickupTimerFinished,
+		SpawnPickupTime);
+}
+
+void APickupSpawnPoint::SpawnPickupTimerFinished()
+{
+	if (HasAuthority())
+	{
+		SpawnPickup();
+	}
+}
+
+void APickupSpawnPoint::SpawnPickup()
+{
+	int32 NumPickupClasses = PickupClasses.Num();
+	if (NumPickupClasses > 0)
+	{
+		int32 Selection = FMath::RandRange(0, NumPickupClasses - 1);
+		SpawnedPickup = GetWorld()->SpawnActor<APickup>(PickupClasses[Selection], GetActorTransform());
+
+		if (HasAuthority() && SpawnedPickup)
+		{
+			SpawnedPickup->OnDestroyed.AddDynamic(this, &ThisClass::StartSpawnPickupTimer);
+		}
+	}
+}
+
+void APickupSpawnPoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Blaster/Source/Blaster/Pickups/PickupSpawnPoint.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PickupSpawnPoint.generated.h"
+
+UCLASS()
+class BLASTER_API APickupSpawnPoint : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APickupSpawnPoint();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	TArray<TSubclassOf<class APickup>> PickupClasses;
+
+	UPROPERTY()
+	APickup* SpawnedPickup = nullptr;
+	
+	UFUNCTION()
+	void StartSpawnPickupTimer(AActor* DestroyedActor);
+	void SpawnPickupTimerFinished();
+	void SpawnPickup();
+
+private:
+	FTimerHandle SpawnPickupTimer;
+
+	UPROPERTY(EditAnywhere)
+	float SpawnPickupTime = 10.f;
+};
